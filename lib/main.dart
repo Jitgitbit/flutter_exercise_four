764379ff63c52756aefced9c8088b125a0f60b48
Jitgitbit <rowowolf@gmail.com>
2020-06-02 19:34:35 +0200

    the !  MultiProvider()  !

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
+
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {  
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) =>                     //-----> version 4.0.0+ needs create i.o. builder method !
-          Products(), 
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Products(),
+        ),
+        ChangeNotifierProvider.value(
+          value: Cart(),
+        ),
+      ],
       child: MaterialApp(
-          title: 'PhoenixFlutterShop',
+          title: 'MyShop',
           theme: ThemeData(
             primarySwatch: Colors.purple,
             accentColor: Colors.deepOrange,
