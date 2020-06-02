1e3c7418c69915ba1bdee4a5e4f70eda7bad7a3b
Jitgitbit <rowowolf@gmail.com>
2020-06-02 10:32:45 +0200

    Working with Providers & Listeners

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,25 @@ import './screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
 import './providers/products_provider.dart';
 
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (ctx) => ProductsProvider(),                 //-----> version 4.0.0+ needs create i.o. builder method !
-          child: MaterialApp(
-        title: 'PhoenixFlutterShop',
-        theme: ThemeData(
-          primarySwatch: Colors.purple,
-          accentColor: Colors.deepOrange,
-          fontFamily: 'Lato',
-        ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        }
-      ),
+      create: (ctx) =>                     //-----> version 4.0.0+ needs create i.o. builder method !
+          ProductsProvider(), 
+      child: MaterialApp(
+          title: 'PhoenixFlutterShop',
+          theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato',
+          ),
+          home: ProductsOverviewScreen(),
+          routes: {
+            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+          }),
     );
   }
 }
