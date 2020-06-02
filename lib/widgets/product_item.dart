b1e961b589d9675c0255e293b7ccf244ad7b1fa7
Jitgitbit <rowowolf@gmail.com>
2020-06-02 20:16:24 +0200

    ! Shopping Cart connected to Badge ! only 1 item for 1 product for now though

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
-
+import '../providers/cart.dart';
 
 class ProductItem extends StatelessWidget {
   // final String id;
@@ -15,7 +15,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
-
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -33,23 +33,28 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: Consumer<Product>(                      //---> with Consumer we now only reload the subpart for favorites, i.o the whole ProductItem Widget Tree !
-            builder: (ctx, provider, _) => IconButton(    //---> the third arg 'child' can be used in case we want to even avoid reloading a subpart within Consumer ! 
-              icon: Icon(                                //---> no need for the child here, so '_' instead !
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
-              onPressed: () {
-                product.toggleFavoriteStatus();
-              },
-              color: Theme.of(context).accentColor,
-            ),
+          leading: Consumer<Product>(                         //---> with Consumer we now only reload the subpart for favorites, i.o the whole ProductItem Widget Tree !
+            builder: (ctx, product, _) => IconButton(        //---> the third arg 'child' can be used in case we want to even avoid reloading a subpart within Consumer ! 
+                  icon: Icon(                               //---> no need for the child here, so '_' instead !
+                    product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                  ),
+                  color: Theme.of(context).accentColor,
+                  onPressed: () {
+                    product.toggleFavoriteStatus();
+                  },
+                ),
           ),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+            },
             color: Theme.of(context).accentColor,
           ),
         ),
