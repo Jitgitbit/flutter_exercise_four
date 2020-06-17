a6bc23349d548f51fd6a93b704d091cd66b12e53
Jitgitbit <rowowolf@gmail.com>
2020-06-17 18:28:41 +0200

    Working on the Shopping Cart & Displaying a Total

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/cart.dart' show Cart;
-import '../widgets/cart_item.dart';
+// import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -32,7 +32,7 @@ class CartScreen extends StatelessWidget {
                     label: Text(
                       '\$${cart.totalAmount}',
                       style: TextStyle(
-                        color: Theme.of(context).primaryTextTheme.title.color,
+                        color: Theme.of(context).primaryTextTheme.headline6.color,
                       ),
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
@@ -47,18 +47,18 @@ class CartScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10),
-          Expanded(
-            child: ListView.builder(
-              itemCount: cart.items.length,
-              itemBuilder: (ctx, i) => CartItem(
-                    cart.items.values.toList()[i].id,
-                    cart.items.keys.toList()[i],
-                    cart.items.values.toList()[i].price,
-                    cart.items.values.toList()[i].quantity,
-                    cart.items.values.toList()[i].title,
-                  ),
-            ),
-          )
+          // Expanded(
+          //   child: ListView.builder(
+          //     itemCount: cart.items.length,
+          //     itemBuilder: (ctx, i) => CartItem(
+          //           cart.items.values.toList()[i].id,
+          //           cart.items.keys.toList()[i],
+          //           cart.items.values.toList()[i].price,
+          //           cart.items.values.toList()[i].quantity,
+          //           cart.items.values.toList()[i].title,
+          //         ),
+          //   ),
+          // )
         ],
       ),
     );
