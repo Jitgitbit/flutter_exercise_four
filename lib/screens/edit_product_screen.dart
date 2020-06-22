a416d0e48079c88218bfd6686e4f391d3fb7dc5e
Jitgitbit <rowowolf@gmail.com>
2020-06-22 13:45:41 +0200

    Multiline Inputs & Disposing Objects

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -9,6 +9,14 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {                                   // ------> you have to use dispose() in order to make sure to clear up that memory after use
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +32,24 @@ class _EditProductScreenState extends State<EditProductScreen> {
               TextFormField(
                 decoration: InputDecoration(labelText: 'Title'),
                 textInputAction: TextInputAction.next,
-                onFieldSubmitted: (_) {                                         // --> actual submit is ignored this way (_)
-                  FocusScope.of(context).requestFocus(_priceFocusNode);        // --> this manages where the input is focused
+                onFieldSubmitted: (_) {                                        // --> actual submit is ignored this way (_)
+                  FocusScope.of(context).requestFocus(_priceFocusNode);       // --> this manages where the input is focused
                 },
               ),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Price'),
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.number,
-                focusNode: _priceFocusNode,                                 // --> focus identified for input, here submit will not be ignored
+                focusNode: _priceFocusNode,                                // --> focus identified for input, here submit will not be ignored           
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,                                   // --> focus identified for input, here submit will not be ignored
               ),
             ],
           ),
