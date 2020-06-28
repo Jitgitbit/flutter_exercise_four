f712f3a20b5bc51fd21390eabab2558fbc0465f0
Jitgitbit <rowowolf@gmail.com>
2020-06-28 16:14:49 +0200

    corrected url

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -68,7 +68,7 @@ class Products with ChangeNotifier {
   // }
 
   Future<void> addProduct(Product product) {
-    const url = 'https://flutter-update.firebaseio.com/products.json';
+    const url = 'https://phoenixfluttershop.firebaseio.com/products.json';
     return http
         .post(
       url,
