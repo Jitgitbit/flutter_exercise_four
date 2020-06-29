1b9691a8eccb52f3fda847474ada33482109ec99
Jitgitbit <rowowolf@gmail.com>
2020-06-29 11:31:29 +0200

    Deleting Utilizing Optimistic Updating

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -138,8 +138,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url = 'https://phoenixfluttershop.firebaseio.com/products/$id.json';
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+      // throw HttpException('Could not delete product.');
+    }
+    existingProduct = null;
   }
 }
