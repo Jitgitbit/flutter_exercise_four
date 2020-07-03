9c97a8d1a83ccd6c78d140a313fd15696ad008a9
Jitgitbit <rowowolf@gmail.com>
2020-07-03 12:02:56 +0200

    Animated the Oder Boxes

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -31,10 +31,10 @@ class ProductItem extends StatelessWidget {
           child: Hero(
               tag: product.id,
               child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
-            ),
+                placeholder: AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
           ),
         ),
         footer: GridTileBar(
